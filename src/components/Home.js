0af6028c06f85d1ca100202d39d7bbf3c7e83057
dreams946 <dreams946@gmail.com>
2023-03-05 12:56:48 +0530

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <div className='container'>
+            <div className='container-left'>
+
+                 <p>lorem ipsu  lorem ipsumm lorem ipsumlorem ipsumlorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+            </div>
+            <div className='container-right'>
+
+            <p>lorem ipsu  lorem ipsumm lorem ipsumlorem ipsumlorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+ 
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
